fecbd831084fdac0fcd66cbc379d713e0df2706b
Alessio Limina <alessio.limina@gmail.com>
2023-01-31 19:44:37 +0100

    Exercise: lists 1

--- src/it/develhope/lists/Start.java
+++ src/it/develhope/lists/Start.java
@@ -10,16 +10,17 @@ import java.util.List;
 
             String[] s1 = new String[]{"Milan", "Rome", "Turin"};
             List<String> cityNames = Arrays.asList(s1);
-            System.out.println(cityNames);
+            System.out.println("The cities are: " + cityNames);
+
         /*
-         * If I run the following line of code, it throws an UnsupportedOperationException.
-         * That's because I can't use '.add()' on a not mutable List.
+         * If I run these following lines of code, it throws an UnsupportedOperationException.
+         * That's because I can't use '.add()' on a not mutable List:
         cityNames.add("Palermo");
         System.out.println(cityNames);
         */
 
             cityNames.set(1, "Telucigalpa");
-            System.out.println(cityNames);
+            System.out.println("The updated cities are: " + cityNames);
 
             System.out.println("-------------Creating a mutable List------------");
 
@@ -31,14 +32,14 @@ import java.util.List;
             kingsOfRome.add("Tarquinio Prisco");
             kingsOfRome.add("Servio Tullio");
             kingsOfRome.add("Tarquinio il Superbo");
-            System.out.println(kingsOfRome);
+            System.out.println("Here we have the seven kings of Rome: " + kingsOfRome);
 
             System.out.println("------Converting the previous list into an array-----");
 
             String[] kingsOfRomeArray = new String[kingsOfRome.size()];
             kingsOfRomeArray = kingsOfRome.toArray(kingsOfRomeArray);
             kingsOfRomeArray[6]= "Lucius Tarquinius Supercar";
-            System.out.println(Arrays.toString(kingsOfRomeArray));
+            System.out.println("And here, the updated list of kings of Rome: " + Arrays.toString(kingsOfRomeArray));
 
 
             System.out.println("---------------------------------------------------------");
